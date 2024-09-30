0483b9892df71c79b4928cbedefeea21b304fd7a
Luan Moreira de Menezes <125929128+LuanMez@users.noreply.github.com>
2024-09-30 16:34:15 -0300

    Delete Logout.php

--- Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destrói todas as variáveis de sessão
-
-// Redireciona para a página de login após logout
-header("Location: login.html");
-exit();
-?>
